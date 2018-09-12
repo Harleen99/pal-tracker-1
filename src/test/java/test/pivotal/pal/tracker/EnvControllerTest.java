7a8b654e971f19e0f7a7afbfa6aa00d67dc1b96e
Harleen Kaur <harleen_kaur@hcl.com>
2018-09-12 10:41:50 +0530

    2nd lab commit

--- src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class EnvControllerTest {
     @Test
     public void getEnv() throws Exception {
-        EnvController controller = new EnvController(
+       EnvController controller = new EnvController(
             "8675",
             "12G",
             "34",
